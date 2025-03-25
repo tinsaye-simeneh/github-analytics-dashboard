1738aa0540faf45399a8925ec4e830eb09fe6719
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2025-03-25 20:06:28 +0300

    chore: Remove unused manifest and touch icon references in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,8 +61,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <title>GitHub Analytics</title>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body>
         <MantineProvider forceColorScheme={colorScheme}>
